f40160b25bdbfb9dd0e3c546bb4a9b1fe0e3d3a9
Samuli Virtanen <samuli.virtanen@tucom.fi>
2018-11-18 16:58:41 +0200

    fixed CR+LF issue in app.js and in server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,12 @@
-'use strict';require('dotenv').config();const http = require ('http');const app = require('./app');const port = process.env.PORT || 3000const server = http.createServer(app);console.log ( `Listening to port ${port} ...` );server.listen(port);
+'use strict';
+
+require('dotenv').config();
+
+const http = require ('http');
+const app = require('./app');
+const port = process.env.PORT || 3000
+const server = http.createServer(app);
+
+console.log ( `Listening to port ${port} ...` );
+
+server.listen(port);
