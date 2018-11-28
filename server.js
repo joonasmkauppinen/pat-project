feaa6ed263056b881f82970020403177d632deaa
Samuli Virtanen <samuli.virtanen@tucom.fi>
2018-11-29 01:15:24 +0200

    added apidoc

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = require('./modules/app');
 const port = process.env.PORT || 3000
 const server = http.createServer(app);
 
+console.log ( `PAT. Server Starting ...` );
 console.log ( `Listening to port ${port} ...` );
 
 server.listen(port);
